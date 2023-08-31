aaa72df3e79d97bde345c75a0ecd44224637a1c1
David Ezeji <ezejidavid@gmail.com>
2023-08-31 20:56:50 +0000

    Update update_s3_test_results.sh

--- update_s3_test_results.sh
+++ update_s3_test_results.sh
@@ -3,8 +3,8 @@
 # this script updates the objects in the s3 bucket "cntf-open5gs-coralogix-test-results" with any changes made to their corresponding local files. 
 
 udpate_s3() {
-   aws s3 cp ./stress_test_logs.txt s3://cntf-open5gs-test-results/stress_test_logs.txt
-   aws s3 cp ./stress_test_error_logs.txt s3://cntf-open5gs-test-results/stress_test_error_logs.txt
+   aws s3 cp ./stress_test_logs.json s3://cntf-open5gs-test-results/stress_test_logs.json
+   aws s3 cp ./stress_test_error_logs.json s3://cntf-open5gs-test-results/stress_test_error_logs.json
 }
 
 udpate_s3
