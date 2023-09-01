5908271e929289efa873491ac5f320a466bda80e
davidezeji <ezejidavid@gmail.com>
2023-09-01 17:57:29 -0500

    added configuration to update tests locally

--- update_s3_test_results.sh
+++ update_s3_test_results.sh
@@ -1,7 +1,9 @@
 #!usr/bin/env bash
 
-# this script updates the objects in the s3 bucket "cntf-open5gs-coralogix-test-results" with any changes made to their corresponding local files. 
+# This reflects new data changes from the pod into a local file
+kubectl cp -n openverso $UE_POD:/tmp/stress_test_logs.json ./stress_test_logs.json
 
+# this script updates the objects in the s3 bucket "cntf-open5gs-coralogix-test-results" with any changes made to their corresponding local files. 
 udpate_s3() {
    aws s3 cp ./stress_test_logs.json s3://cntf-open5gs-test-results/stress_test_logs.json
    aws s3 cp ./stress_test_error_logs.json s3://cntf-open5gs-test-results/stress_test_error_logs.json
